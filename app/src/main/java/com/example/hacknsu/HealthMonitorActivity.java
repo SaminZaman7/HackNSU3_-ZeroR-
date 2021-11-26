cd0d2ff94854a2ddd74b00254d9c1c7f946f0e1b
Fahim Istiak <fahim.istiak@northsouth.edu>
2021-11-26 15:31:17 +0600

    UI added

---
+++ app/src/main/java/com/example/hacknsu/HealthMonitorActivity.java
@@ -0,0 +1,14 @@
+package com.example.hacknsu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HealthMonitorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_health_monitor);
+    }
+}
