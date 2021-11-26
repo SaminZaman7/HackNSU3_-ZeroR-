dd7376a9fe39264aef160d0199b7e5f72df50159
Fahim Istiak <fahim.istiak@northsouth.edu>
2021-11-26 13:12:17 +0600

    login page added

--- app/src/main/java/com/example/hacknsu/LoginActivity.java
+++ app/src/main/java/com/example/hacknsu/LoginActivity.java
@@ -1,14 +1,227 @@
 package com.example.hacknsu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.security.PrivateKey;
 
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText mEmail, mPassword;
+    Button mLoginBtn;
+    TextView mCreateHere;
+    ProgressBar progressbar;
+    FirebaseAuth fAuth;
+    Button mGoogle;
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
+    private GoogleSignInClient mGoogleSignInClient;
+    private final static int RC_SIGN_IN = 123;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser user = fAuth.getCurrentUser();
+        if (user != null) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        createRequest();
+        mGoogle = findViewById(R.id.btn_google);
+
+
+        mEmail = findViewById(R.id.email);
+        mPassword = findViewById(R.id.pass);
+        mLoginBtn = findViewById(R.id.btn_login);
+        mCreateHere = findViewById(R.id.createheretext);
+        progressbar = findViewById(R.id.progressBar);
+        fAuth = FirebaseAuth.getInstance();
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    mEmail.setError("Email is required. ");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    mPassword.setError("Password is required. ");
+                    return;
+                }
+                progressbar.setVisibility(View.VISIBLE);
+
+
+                fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(!task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this, "Login Error , Please Log In Again ", Toast.LENGTH_SHORT).show();
+
+                        }
+                        else
+                        {
+                            //email verification check
+                            if(fAuth.getCurrentUser().isEmailVerified()) {
+                                Intent intToHome = new Intent(LoginActivity.this, MainActivity.class);
+                                startActivity(intToHome);
+                            }
+                            else {
+                                Toast.makeText(LoginActivity.this, "Please Verify Your Email ! ", Toast.LENGTH_SHORT).show();
+
+                            }
+                        }
+
+                    }
+                });
+            }
+        });
+
+
+        mGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+
+        mCreateHere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+    }
+
+    private void createRequest() {
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+//                Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+//                Log.w(TAG, "Google sign in failed", e);
+                Toast.makeText(LoginActivity.this, "Error! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        fAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+//                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = fAuth.getCurrentUser();
+                            updateUI(user);
+                            Toast.makeText(LoginActivity.this, "Successful ", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            updateUI(null);
+                            Toast.makeText(LoginActivity.this, "Error! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            progressbar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+    }
+    // update ui state for user
+    private void updateUI(FirebaseUser fUser) {
+        //firebase database reference
+        rootNode= FirebaseDatabase.getInstance();
+        reference=rootNode.getReference("users");
+        GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
+
+        if (signInAccount != null) {
+            String personName = signInAccount.getDisplayName();
+            String personEmail = signInAccount.getEmail();
+            String uid= fAuth.getUid();
+            String Image_Url = signInAccount.getPhotoUrl().toString();
+            LoginHelperClass helperClass = new LoginHelperClass(personName, personEmail, uid);
+            Toast.makeText(LoginActivity.this, personName + personEmail, Toast.LENGTH_SHORT).show();
+
+            reference.child(uid).setValue(helperClass);
+            reference.child(uid).child("Img_url").setValue(Image_Url);
+        }
+        Intent ih = new Intent(LoginActivity.this,MainActivity.class);
+        startActivity(ih);
+
+
+
+        
+
+
+
+
     }
 }
--- app/src/main/java/com/example/hacknsu/MapActivity.java
+++ app/src/main/java/com/example/hacknsu/MapActivity.java
@@ -1,14 +1,39 @@
 package com.example.hacknsu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
-public class MapActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap hospitalmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        hospitalmap = googleMap;
+        LatLng evercare = new LatLng(23.810377, 90.431149);
+        hospitalmap.addMarker(new MarkerOptions().position(evercare).title("Hospital"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(evercare));
+
     }
 }
